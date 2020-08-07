6646a234ecddceaa16d884ae086688d78d681681
qinxiaokai <qinxiaokai@qihoinfo.com>
2020-08-07 17:42:04 +0800

    更新

--- deploy.config.js
+++ deploy.config.js
@@ -8,20 +8,20 @@ module.exports = {
     script: "npm run build", // 测试环境打包脚本
     host: '', // 测试服务器地址
     port: 22, // ssh port，一般默认22
-    username: '', // 登录服务器用户名
+    username: 'root', // 登录服务器用户名
     password: '', // 登录服务器密码
     distPath: 'dist',  // 本地打包dist目录
-    webDir: '',  // // 测试环境服务器地址
+    webDir: 'cse',  // // 测试环境服务器地址
   },
   prod: {  // 线上环境
     name: '线上环境',
     script: "npm run build", // 线上环境打包脚本
     host: '', // 线上服务器地址
     port: 22, // ssh port，一般默认22
-    username: '', // 登录服务器用户名
+    username: 'root', // 登录服务器用户名
     password: '', // 登录服务器密码
     distPath: 'dist',  // 本地打包dist目录
-    webDir: '' // 线上环境web目录
+    webDir: 'cse' // 线上环境web目录
   }
   // 再还有多余的环境按照这个格式写即可
 }
